32d3e31063c5d736736a80fc616369d2ce5ded9c
Jan Berkel <jan.berkel@gmail.com>
2011-05-17 11:57:02 +0200

    Update javadoc shell script

---
+++ update_javadoc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## updates the javadocs hosted at 
+## http://soundcloud.github.com/java-api-wrapper/javadoc/com/soundcloud/api/package-summary.html
+## by regenerating them via gradle and pushing them back to gh-pages
+
+DOCS=docs
+
+rm -rf $DOCS && \
+git clone git@github.com:soundcloud/java-api-wrapper.git $DOCS -b gh-pages && \
+gradle javadoc && \
+rsync -f 'exclude .git' -rv --delete build/docs/ $DOCS
+cd $DOCS &&
+git commit -m 'javadoc update' -a &&
+git add . &&
+git commit --amend -a -m 'javadoc update' &&
+git push origin gh-pages &&
+rm -rf $DOCS
