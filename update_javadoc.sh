9e9282f8a7b4ce5a5028789fcc64067bd3dfa67c
Jan Berkel <jan.berkel@gmail.com>
2011-05-17 12:05:07 +0200

    Simplify

--- update_javadoc.sh
+++ update_javadoc.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-## updates the javadocs hosted at 
+## updates the javadocs hosted at
 ## http://soundcloud.github.com/java-api-wrapper/javadoc/com/soundcloud/api/package-summary.html
 ## by regenerating them via gradle and pushing them back to gh-pages
 
 DOCS=docs
 
-rm -rf $DOCS && \
-git clone . $DOCS -b gh-pages && \
-gradle javadoc && \
+set -e
+
+rm -rf $DOCS
+git clone . $DOCS -b gh-pages
+gradle javadoc
 rsync -f 'exclude .git' -rv --delete build/docs/ $DOCS
-cd $DOCS &&
-git commit -m 'javadoc update' -a &&
-git add . &&
-git commit --amend -a -m 'javadoc update' &&
-git push origin gh-pages &&
+cd $DOCS
+git commit -m 'javadoc update' -a
+git add .
+git commit --amend -a -m 'javadoc update'
+git push origin gh-pages
 rm -rf $DOCS
